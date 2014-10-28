9aa3f6e569c7bcbbcaf82a94fbe76bcdc9403068
y3sh <joshhibschman@gmail.com>
2014-10-28 09:25:05 -0500

    restful api intro

--- restful-api/app.js
+++ restful-api/app.js
@@ -10,6 +10,8 @@ if (process.env.ENV === "prod") {
   app.set('port', 3000);
 }
 
+console.log("here")
+
 app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
