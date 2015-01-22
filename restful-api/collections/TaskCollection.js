734a3e337dfcf4cf4a4617122bb1dbcdbcd122de
y3sh <joshhibschman@gmail.com>
2015-01-22 10:23:16 -0600

    Structure change, comments

--- restful-api/collections/TaskCollection.js
+++ restful-api/collections/TaskCollection.js
@@ -1,3 +1,7 @@
+/*
+ A simple structure for a list of Backbone models. See http://backbonejs.org/#Collection
+ */
+
 var Backbone = require("backbone");
 var TaskModel = require("../models/TaskModel");
 
